3df62d1100ffb730df212b591fb4dcbfba1a6079
Emmalee <emilee.wimmer@gmail.com>
2022-12-08 14:55:20 -0700

    dded logic to navtabs and portfolio container

---
+++ src/components/NavTabs.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function NavTabs({currentPage, handlePageChange}) {
+  return(
+    // <div> Emm's Portfolio</div>
+    <ul className= "nav nav-tabs">
+    <li className= "nav-item">
+    <a href= "#about" onClick = {() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+    >
+    About </a>
+    </li>
+
+    <li className= "nav-item">
+    <a href= "#portfolio" onClick = {() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+    >
+    About </a>
+    </li>
+
+    <li className= "nav-item">
+    <a href= "#resume" onClick = {() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+    >
+    About </a>
+    </li>
+    <li className= "nav-item">
+    <a href= "#contact" onClick = {() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+    >
+    About </a>
+    </li>
+    </ul>
+
+  );
+}
+
+export default NavTabs;
